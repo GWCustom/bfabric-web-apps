5a42a5257c97c1e410a9cd2a7226e7c4d5a2429a
MarcZuberGWC <marc@gwcustom.com>
2025-01-09 02:17:05 +0100

    add comments

--- bfabric_web_apps/utils/get_logger.py
+++ bfabric_web_apps/utils/get_logger.py
@@ -2,7 +2,7 @@ from bfabric_web_apps.objects.Logger import Logger
 
 def get_logger(token_data):
 
-    # Extract logging-related information from token_data, with defaults for missing values
+    """ Extract logging-related information from token_data, with defaults for missing values """
     jobId = token_data.get('jobId', None)
     username = token_data.get("user_data", "None")
     environment = token_data.get("environment", "None")
