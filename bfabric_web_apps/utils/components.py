b075415dfa9b84a351b33d8a8e1c829e0e9d7e57
Griffin <griffin@gwc1.localdomain>
2025-07-24 14:40:15 +0200

    parameterized bfabric host for test and prod in config / env spec

--- bfabric_web_apps/utils/components.py
+++ bfabric_web_apps/utils/components.py
@@ -1,16 +1,17 @@
 from dash import html
 import dash_daq as daq
+from bfabric_web_apps.utils.config import settings
 
 DEVELOPER_EMAIL = "gwhite@fgcz.ethz.ch"
 
 expired = [
     html.P("Your session has expired. Please log into bfabric to continue:"),
-    html.A('Login to Bfabric', href='https://fgcz-bfabric.uzh.ch/bfabric/')
+    html.A('Login to Bfabric', href=f'https://{settings.PRODUCTION_BFABRIC_DOMAIN}/bfabric/')
 ]
 
 no_entity = [
     html.P("There was an error fetching the data for your entity. Please try accessing the applicaiton again from bfabric:"),
-    html.A('Login to Bfabric', href='https://fgcz-bfabric.uzh.ch/bfabric/')
+    html.A('Login to Bfabric', href=f'https://{settings.PRODUCTION_BFABRIC_DOMAIN}/bfabric/')
 ]
 
 dev = [html.P("This page is under development. Please check back later."),html.Br(),html.A("email the developer for more details",href="mailto:"+DEVELOPER_EMAIL)]
@@ -19,7 +20,7 @@ auth = [html.Div(id="auth-div")]
 
 no_auth = [
     html.P("You are not currently logged into an active session. Please log into bfabric to continue:"),
-    html.A('Login to Bfabric', href='https://fgcz-bfabric.uzh.ch/bfabric/')
+    html.A('Login to Bfabric', href=f'https://{settings.PRODUCTION_BFABRIC_DOMAIN}/bfabric/')
 ]
 
 charge_switch = [
